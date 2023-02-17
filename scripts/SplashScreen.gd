b3e1e3eb68eebfef6b57a2746fc6250f2de8921a
Max Malm <benjick@dumfan.net>
2023-02-18 00:48:46 +0100

    More SplashScreen

--- scripts/SplashScreen.gd
+++ scripts/SplashScreen.gd
@@ -1,10 +1,12 @@
 extends Control
 
-@onready var animated_sprite_2d = $Node2D/AnimatedSprite2D
+@onready var king = $Sprites/King
+@onready var pig = $Sprites/Pig
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animated_sprite_2d.play("Idle")
+	king.play("Idle")
+	pig.play("Idle")
 	pass # Replace with function body.
 
 
